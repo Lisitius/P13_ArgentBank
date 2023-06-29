071e4399a26e8ac2bf27943743f3ac09b58472d6
Lisitius <benjamin.fichaux@gmail.com>
2023-06-29 08:45:27 +0200

    Hero, features & footer

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "../../sass/layouts/_footer.scss";
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <p class="footer__text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import "../../sass/pages/_home.scss";
-import Header from "../../layouts/header/Header";
-import Hero from "../../layouts/Hero/Hero";
+import Header from "../../layouts/Header/Header";
+import Hero from "../../components/Home/Hero/Hero";
+import Features from "../../components/Home/Features/Features";
+import Footer from "../../layouts/Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <Features />
+      <Footer />
     </div>
   );
 };
